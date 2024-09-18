462c3359ac77d867a4c38186c08b71efa5f3fd88
Ilya Aizenberg <ilyaizen@gmail.com>
2024-09-18 20:16:25 +0300

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,19 +1,17 @@
 { pkgs }: {
   deps = [
     pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
+    pkgs.replitPackages.prybar-python3
     pkgs.replitPackages.stderred
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      pkgs.glib
-      pkgs.xorg.libX11
+      pkgs.python310Full
     ];
     PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python3}/bin/prybar-python3";
   };
 }
